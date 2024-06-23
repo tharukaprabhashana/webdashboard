de40eb24b0338fcd8debe6e7dbf1f6b95c3b4e63
Tharuka Prabhashana <mt.prabhashana@gmail.com>
2024-06-23 15:59:05 +0530

    UI updated

--- firebase-dashboard/src/App.jsx
+++ firebase-dashboard/src/App.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import TemperatureGraph from './components/TemperatureGraph';
 import Map from './components/Map';
 import LatestData from './components/LatestData';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <h1>Bag Data Dashboard</h1>
             <LatestData />
             <TemperatureGraph />
