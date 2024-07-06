1861fba74e659f02e1e43d78296e2ea44998b12f
Judith Fernando <judithfdo2002@gmail.com>
2024-07-06 23:57:53 +0530

    signUp&signIn authentication
    SignUp and SignIn authentication through firebase is completed

--- firebase-dashboard/src/components/LoginSignup/SignIn.jsx
+++ firebase-dashboard/src/components/LoginSignup/SignIn.jsx
@@ -1,100 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase'; // Import Firebase auth
 
-    
-
-function SignIn() { 
-
+function SignIn() {
+  const [error, setError] = useState(''); // Initialize useState
   const navigator = useNavigate();
 
-    const clickSignUp = ()=>{
-        navigator("/SignUp");
-    }
-    const onSignIn = (e) => {
+  const clickSignUp = () => {
+    navigator("/SignUp");
+  }
 
-      e.preventDefault();
+  const onSignIn = (e) => {
+    e.preventDefault();
 
-      const email = document.getElementById('login-email').value;
-      const password = document.getElementById('login-password').value;
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
 
-      auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log('User signed in:', cred.user);  
+    auth.signInWithEmailAndPassword(email, password)
+      .then(cred => {
+        console.log('User signed in:', cred.user);
         navigator("/");
-      }).catch(err => {
-          console.error("Error signing in:", err);
+      })
+      .catch(err => {
+        console.error("Error signing in:", err);
+        if (err.code === 'auth/user-not-found') {
+          setError("User not found. Please sign up first.");
+        } else {
+          setError("Error signing in. Please check your credentials.");
+        }
       });
   }
 
-
   return (
     <div className='login-signup-container' id="modal-login">
-    <div className='container'>
+      <div className='container'>
         <div className='left'>
-            <>
-            <h1 style={{textAlign:'center',color:'rgba(25, 25, 113, 0.933)'}}>Sign in</h1>
-            </>
+          <>
+            <h1 style={{textAlign: 'center', color: 'rgba(25, 25, 113, 0.933)'}}>Sign in</h1>
+          </>
 
-            <form id="login-form">
+          <form id="login-form">
             <div className='input'>
-            <>
-            <img src='https://img.icons8.com/?size=100&id=77883&format=png&color=000000' style={{width:'20px'}} />
-            <input type='text' id="login-email" placeholder="Email" style={{width:'50%',height:'30px',border:'none',borderRadius:'4px ',boxShadow:' 0 1px 2px rgba(0, 0, 0, 0.56)'}}  /><br />
-            </>
-            <br />
-            <>
-            <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width:'20px'}}/>
-            <input type='password' id="login-password" placeholder="Password" style={{width:'50%',height:'30px',border:'none',borderRadius:'4px ',boxShadow:' 0 1px 2px rgba(0, 0, 0, 0.56)'}}/>
-            </>
-            <br /><br />
-            <>
-            <button
-            style={{
-                margin:'auto',
-                width: '80px',
-                height: '30px',
-                backgroundColor: 'rgba(25, 25, 113, 0.933)',
-                color: 'white',
-                border: 'none',
-                borderRadius: '4px',
-                fontSize: '16px',
-                cursor: 'pointer'
-              }} onClick={(e)=>onSignIn(e)}
-            >Sign in</button>
-            </> 
+              <>
+                <img src='https://img.icons8.com/?size=100&id=77883&format=png&color=000000' style={{width: '20px'}} />
+                <input type='text' id="login-email" placeholder="Email" style={{width: '50%', height: '30px', border: 'none', borderRadius: '4px', boxShadow: ' 0 1px 2px rgba(0, 0, 0, 0.56)'}} /><br />
+              </>
+              <br />
+              <>
+                <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width: '20px'}}/>
+                <input type='password' id="login-password" placeholder="Password" style={{width: '50%', height: '30px', border: 'none', borderRadius: '4px', boxShadow: ' 0 1px 2px rgba(0, 0, 0, 0.56)'}}/>
+              </>
+              <br /><br />
+              <>
+                {error && <p style={{ color: 'red' ,margin: 20 }}>{error}</p>}
+                <button
+                  style={{
+                    margin: 'auto',
+                    width: '80px',
+                    height: '30px',
+                    backgroundColor: 'rgba(25, 25, 113, 0.933)',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    fontSize: '16px',
+                    cursor: 'pointer'
+                  }} onClick={(e) => onSignIn(e)}
+                >Sign in</button>
+              </>
             </div>
-            </form>
-            
-
+          </form>
         </div>
         <div className='right'>
-            <>
-            <h1>Hello,Friend!</h1>
-            </>
-            <>
+          <>
+            <h1>Hello, Friend!</h1>
+          </>
+          <>
             <p>Enter your personal details and start <br /> journey with us</p>
-            
-            </>
-            <br /> 
-            
-            <button 
+          </>
+          <br />
+          <button
             style={{
-                width: '80px',
-                height: '30px',
-                backgroundColor: 'white',
-                color: 'rgba(25, 25, 113, 0.933)',
-                border: 'none',
-                borderRadius: '4px',
-                fontSize: '16px',
-                cursor: 'pointer',
-                margin: "auto"
-              }} onClick={()=>clickSignUp()}
-            >Sign up</button>
+              width: '80px',
+              height: '30px',
+              backgroundColor: 'white',
+              color: 'rgba(25, 25, 113, 0.933)',
+              border: 'none',
+              borderRadius: '4px',
+              fontSize: '16px',
+              cursor: 'pointer',
+              margin: "auto"
+            }} onClick={() => clickSignUp()}
+          >Sign up</button>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
 
-
-export default SignIn
+export default SignIn;
--- firebase-dashboard/src/components/LoginSignup/SignUp.jsx
+++ firebase-dashboard/src/components/LoginSignup/SignUp.jsx
@@ -1,112 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './LoginSignup.css';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase'; // Import Firebase auth
 
-
 function SignUp() {
-    const navigator = useNavigate();
-    const clickSignIn = ()=>{
-        navigator("/SignIn");
-    }
+  const [error, setError] = useState(''); // Initialize useState for error message
+  const navigate = useNavigate();
 
-    const onSignUp = (e) => {
-      e.preventDefault();
-      // get user info
-      const email = document.getElementById('signup-email').value;
-      const password = document.getElementById('signup-password').value;
-      const conpassword = document.getElementById('confirm-signup-password').value;
-
-
-      if(password == conpassword){
-        // sign up the user
-        auth.createUserWithEmailAndPassword(email, password, conpassword)
-        .then(cred => {
-          console.log('User signed up:', cred.user);  
-          navigator("/");
-        }).catch(err => {
-            console.error("Error signing up:", err);
-        });
-      }else{
-        alert("Password doesn't match the confirm password!");
-      }
-
-      
+  const clickSignIn = () => {
+    navigate("/SignIn");
   }
-    
+
+  const onSignUp = (e) => {
+    e.preventDefault();
+    // get user info
+    const email = document.getElementById('signup-email').value;
+    const password = document.getElementById('signup-password').value;
+    const conpassword = document.getElementById('confirm-signup-password').value;
+
+    if (password === conpassword) {
+      // sign up the user
+      auth.createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+          console.log('User signed up:', cred.user);
+          navigate("/");
+        })
+        .catch(err => {
+          console.error("Error signing up:", err);
+          setError("Error signing up: " + err.message); // Set the error message state
+        });
+    } else {
+      setError("Password doesn't match the confirm password!"); // Set the error message state
+    }
+  }
+
   return (
     <div className='login-signup-container' id="modal-signup">
-    <div className='container'>
+      <div className='container'>
         <div className='left'>
-            <>
-            <h1 style={{textAlign:'center',color:'rgba(25, 25, 113, 0.933)'}}>Sign up</h1>
-            </>
+          <>
+            <h1 style={{textAlign: 'center', color: 'rgba(25, 25, 113, 0.933)'}}>Sign up</h1>
+          </>
 
-            <form id="signup-form">
+          <form id="signup-form">
             <div className='input'>
-            <>
-            <img src='https://img.icons8.com/?size=100&id=77883&format=png&color=000000' style={{width:'20px'}} />
-             
-          <input type="email" id="signup-email" placeholder="Email" style={{width:'50%',height:'30px',border:'none',borderRadius:'4px ',boxShadow:' 0 1px 2px rgba(0, 0, 0, 0.56)'}}  /><br />
-            </>
-            <br />
-            <>
-            <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width:'20px'}}/>
-            <input type='password' id="signup-password" placeholder="Password" style={{width:'50%',height:'30px',border:'none',borderRadius:'4px ',boxShadow:' 0 1px 2px rgba(0, 0, 0, 0.56)'}}/>
-            </>
-            <br /><br />
-            <>
-            <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width:'20px'}}/>
-            <input type='password' id="confirm-signup-password" placeholder="Confirm Password" style={{width:'50%',height:'30px',border:'none',borderRadius:'4px ',boxShadow:' 0 1px 2px rgba(0, 0, 0, 0.56)'}}/>
-            </>
-            <br /><br />
-            <>
-            <button
-            style={{
-                width: '80px',
-                height: '30px',
-                backgroundColor: 'rgba(25, 25, 113, 0.933)',
-                color: 'white',
-                border: 'none',
-                borderRadius: '4px',
-                fontSize: '16px',
-                cursor: 'pointer',
-                margin: '10px auto',
-              }} onClick={(e)=>onSignUp(e)}
-            >Sign up</button>
-            </> 
+              <>
+                <img src='https://img.icons8.com/?size=100&id=77883&format=png&color=000000' style={{width: '20px'}} />
+                <input type="email" id="signup-email" placeholder="Email" style={{width: '50%', height: '30px', border: 'none', borderRadius: '4px', boxShadow: ' 0 1px 2px rgba(0, 0, 0, 0.56)'}} /><br />
+              </>
+              <br />
+              <>
+                <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width: '20px'}} />
+                <input type='password' id="signup-password" placeholder="Password" style={{width: '50%', height: '30px', border: 'none', borderRadius: '4px', boxShadow: ' 0 1px 2px rgba(0, 0, 0, 0.56)'}} />
+              </>
+              <br /><br />
+              <>
+                <img src='https://img.icons8.com/?size=100&id=59855&format=png&color=000000' style={{width: '20px'}} />
+                <input type='password' id="confirm-signup-password" placeholder="Confirm Password" style={{width: '50%', height: '30px', border: 'none', borderRadius: '4px', boxShadow: ' 0 1px 2px rgba(0, 0, 0, 0.56)'}} />
+              </>
+              <br /><br />
+              <>
+                {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
+                <button
+                  style={{
+                    width: '80px',
+                    height: '30px',
+                    backgroundColor: 'rgba(25, 25, 113, 0.933)',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    fontSize: '16px',
+                    cursor: 'pointer',
+                    margin: '10px auto',
+                  }} onClick={(e) => onSignUp(e)}
+                >Sign up</button>
+              </>
             </div>
-            </form>
-            
-
+          </form>
         </div>
         <div className='right'>
-            <>
-            <h1>Hello,Friend!</h1>
-            </>
-            <>
+          <>
+            <h1>Hello, Friend!</h1>
+          </>
+          <>
             <p>If you have an account Sign in</p>
-            </>
-            <br /> 
-            
-            <button 
+          </>
+          <br />
+          <button
             style={{
-                width: '80px',
-                height: '30px',
-                backgroundColor: 'white',
-                color: 'rgba(25, 25, 113, 0.933)',
-                border: 'none',
-                borderRadius: '4px',
-                fontSize: '16px',
-                cursor: 'pointer',
-                margin:'10px auto'
-              }} onClick={()=>clickSignIn()}
-            >Sign in</button>
+              width: '80px',
+              height: '30px',
+              backgroundColor: 'white',
+              color: 'rgba(25, 25, 113, 0.933)',
+              border: 'none',
+              borderRadius: '4px',
+              fontSize: '16px',
+              cursor: 'pointer',
+              margin: '10px auto'
+            }} onClick={() => clickSignIn()}
+          >Sign in</button>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
 
-
-export default SignUp
+export default SignUp;
